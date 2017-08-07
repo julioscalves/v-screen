a38b313799b7c0cdc25ff68161c2b2890db8e16d
julioscalves <julioscalves@gmail.com>
2017-08-07 16:29:07 -0300

    Create log.py

---
+++ log.py
@@ -0,0 +1,23 @@
+import glob
+import pandas as pd
+
+logs = [log for log in glob.glob("*.log")]
+
+dataset = {"id": [],
+           "pos": [],
+           "affinity (kcal/mol)": [],
+           "rmsd l.b.": [],
+           "rmsd u.b.": []}
+
+for log in logs:
+    with open(log) as dock:
+        for line in dock.readlines():
+            if line.startswith("  ") and line.split()[0] != "|":
+                dataset["id"].append(log[:-4])
+                dataset["pos"].append(line.split()[0])
+                dataset["affinity (kcal/mol)"].append(line.split()[1])
+                dataset["rmsd l.b."].append(line.split()[2])
+                dataset["rmsd u.b."].append(line.split()[3])
+
+dataframe = pd.DataFrame(data=dataset)
+dataframe.to_csv("docks.csv")
